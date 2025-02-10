a0b4cdadfa05c860c1f87965313bd4223349cc36
Joe Egboka <joeegboka@gmail.com>
2025-02-10 23:25:14 +0100

    create changelog sdk

---
+++ changelog-sdk/src/sdk.ts
@@ -0,0 +1,132 @@
+export type SDKOptions = {
+  onSuccess: (data: any) => void;
+  onError: (error: Error) => void;
+  onClose: () => void;
+};
+
+export class SDK {
+  onSuccess!: SDKOptions["onSuccess"];
+  onError!: SDKOptions["onError"];
+  onClose!: SDKOptions["onClose"];
+  config!: Record<string, any>;
+
+  constructor({ onSuccess, onError, onClose, ...rest }: SDKOptions) {
+    // singleton instance initialization
+    if (!(this instanceof SDK)) {
+      return new SDK({ onSuccess, onError, onClose, ...rest });
+    }
+    this.onSuccess = onSuccess;
+    this.onError = onError;
+    this.onClose = onClose;
+    // other configuration
+    this.config = rest;
+  }
+
+  init() {
+    this.#openIframe();
+    this.#addMessageListener();
+  }
+
+  // private method that opens the iframe with a provided URL
+  #openIframe() {
+    // create the modal backdrop
+    const modalBackdrop = document.createElement("div");
+    modalBackdrop.classList.add("sdk-modal-backdrop");
+    modalBackdrop.style.position = "fixed";
+    modalBackdrop.style.top = "0";
+    modalBackdrop.style.left = "0";
+    modalBackdrop.style.width = "100%";
+    modalBackdrop.style.height = "100%";
+    modalBackdrop.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+    modalBackdrop.style.zIndex = "9999";
+    modalBackdrop.style.display = "flex";
+    modalBackdrop.style.justifyContent = "center";
+    modalBackdrop.style.alignItems = "center";
+
+    // create modal container
+    const modalContainer = document.createElement("div");
+    modalContainer.style.borderRadius = "5px";
+    modalContainer.style.position = "relative";
+    modalContainer.style.width = "100%";
+    modalContainer.style.height = "100%";
+
+    // create loader element
+    const loader = document.createElement("div");
+    loader.textContent = "Loading...";
+    loader.style.textAlign = "center";
+    loader.style.padding = "20px";
+    loader.style.fontWeight = "bold";
+    loader.style.color = "white";
+    loader.style.height = "100vh";
+    loader.style.display = "flex";
+    loader.style.alignItems = "center";
+    loader.style.justifyContent = "center";
+
+    modalContainer.appendChild(loader);
+    modalBackdrop.appendChild(modalContainer);
+    document.body.appendChild(modalBackdrop);
+
+    // create iframe element
+    const iframe = document.createElement("iframe");
+    // iframe.src = this.config.url;
+    iframe.src = "http://localhost:5173/changelogs";
+
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+
+    // when the iframe is loaded, send the config data to the iframe
+    iframe.onload = () => {
+      iframe.contentWindow?.postMessage(
+        { type: "changelogData", config: this.config },
+        `${process.env.TARGET_ORIGIN}`
+      );
+
+      // remove the loader and show the iframe
+      modalContainer.removeChild(loader);
+      modalContainer.appendChild(iframe);
+      modalContainer.style.display = "block";
+    };
+
+    // handle iframe loading error
+    iframe.onerror = (error) => {
+      loader.textContent = "Error loading the iframe";
+      console.error(error);
+      setTimeout(() => {
+        this.#closeIframe();
+      }, 2000);
+    };
+  }
+
+  // private method that listens to messages from the iframe
+  #addMessageListener() {
+    window.addEventListener("message", (event) => {
+      const { data } = event;
+      // check event origin
+      if (event.origin !== process.env.TARGET_ORIGIN) {
+        return;
+      }
+
+      if (event.data?.type === "close") {
+        this.#closeIframe();
+      }
+      // handle data from sdk
+      if (event.data?.type === "success") {
+        this.onSuccess(data);
+      }
+    });
+  }
+
+  // private method that closes the iframe
+  #closeIframe() {
+    // remove the modal backdrop
+    const modalBackdrop = document.querySelector(".sdk-modal-backdrop");
+    if (modalBackdrop) {
+      document.body.removeChild(modalBackdrop);
+    }
+  }
+
+  sendToHost(data: any) {
+    window.parent.postMessage(data, "*");
+  }
+}
